eeb0054d85d673e6464fcd65a196d6ac9ecd8a7f
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-11-09 12:45:21 +0200

    connected errorsHandler middleware to the server, removed express.json middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,16 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import cors from 'cors';
 import { ENV, Api } from './common/enums'
 import { playersData } from './routes';
+import { errorsHandler } from './middlewares/error'
 
 const app: Express = express();
 
-app
-    .use(cors({ origin: "*" }))
-    .use(express.json());
+app.use(cors({ origin: "*" }));
 
 app.use(Api.BASE, playersData);
 
+app.use(errorsHandler);
 
 app.listen(ENV.PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${ENV.PORT}`);
