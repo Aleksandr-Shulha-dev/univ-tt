5dd3d321bf2af3b6c9c5aa7c91f2707f1cd9e25a
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-11-09 12:43:07 +0200

    created errorsHandler middleware and HttpError class

---
+++ backend/src/middlewares/error/index.ts
@@ -0,0 +1,26 @@
+import { HttpError } from '../../dto';
+import httpStatus from 'http-status-codes';
+
+import type { NextFunction, Request, Response } from 'express';
+
+export const errorsHandler = (
+  error: Error | HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
+  if (res.headersSent) {
+    return next(error);
+  }
+
+  if (error instanceof HttpError) {
+    res.status(error.code).json({ message: error.message });
+    return;
+  }
+
+  res.status(httpStatus.BAD_REQUEST).json({
+    error,
+    message: error.message,
+  });
+  return;
+};
