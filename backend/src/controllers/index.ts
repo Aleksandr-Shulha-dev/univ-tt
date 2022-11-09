250992f90fb5dbe990aa5afc2e705e7927a5a51a
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-11-09 12:40:37 +0200

    created getPlayer controller

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
+import { getFakePlayer } from '../services';
+import { createStreamString } from '../helper'
 
 export const getPlayers = (
   req: Request,
@@ -6,6 +8,27 @@ export const getPlayers = (
   next: NextFunction
 ) => {
   try {
+    const closeConnection = () => {
+      clearInterval(intervalId);
+      res.end();
+    }
+
+    const stream = (maxCount: number) => {
+      let count = 0;
+      return () => {
+        if(count > maxCount) {
+          res.write(createStreamString());
+          closeConnection();
+          return;
+        }
+        const data = getFakePlayer();
+        res.write(createStreamString(data))
+        count++;
+      }
+    }
+
+    let intervalId = setInterval(stream(6), 3000);
+    res.on('close', closeConnection);
 
   } catch(error) {
     next(error);
