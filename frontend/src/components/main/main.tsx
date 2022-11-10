d7be0c2e7652e8aa5e6ee666afd68b0f5c52baf7
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-11-10 12:18:23 +0200

    added broad component and title to main component and added styles

--- frontend/src/components/main/main.tsx
+++ frontend/src/components/main/main.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react';
+import { Board } from '../board/board';
 
 import './styles.scss';
  
 const Main: FC = () => {
   return (
     <main className="main">
+      <h1 className="title">Score board</h1>
+      <Board />
     </main>
   );
 }
