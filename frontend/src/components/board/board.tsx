c2e81bd878115694cc259903892c2da68019d856
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-11-10 12:17:29 +0200

    created broad component, implemented logic, added styles and interface for props

---
+++ frontend/src/components/board/board.tsx
@@ -0,0 +1,40 @@
+import { FC, useEffect } from 'react';
+import { updateLocalStorage } from '../../helpers';
+import { useAppDispatch, useAppSelector } from '../../hooks/store' 
+import { setPlayer } from '../../store/slice';
+import { EmptyBoard } from '../empty-board/empty-board';
+import { Table } from '../table/table';
+
+import './styles.scss';
+ 
+const Board: FC = () => {
+  const { playerList } = useAppSelector((state) => state.players);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    return () => {
+      const evtSource = new EventSource("http://localhost:8001/stream");
+      evtSource.onmessage = (message) => {
+        if(message.lastEventId === "-1") {
+          evtSource.close();
+          return;
+        }
+        const newPlayer = JSON.parse(message.data);
+        dispatch(setPlayer(newPlayer));
+        updateLocalStorage(newPlayer);
+      }
+    }
+  }, [])
+
+  return (
+    <div className="board">
+      {
+        playerList.length === 0 ?
+          <EmptyBoard /> :
+          <Table />
+      }
+    </div>
+  );
+}
+ 
+export { Board };
